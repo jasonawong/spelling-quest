ed90eb3e6ed1e982d082e5d41f265444817b090d
Jason Wong <jasonwong@Jasons-MacBook-Air.local>
2026-08-22 00:13:46 -0400

    Add premier spelling activities and Bridge Builder

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'spelling-quest-v22';
+const CACHE = 'spelling-quest-v45';
 const DETECTIVE_WORDS = ['wrench','hello','yellow','bread','kept','better','sentence','eggshell','medal','metal','remember','reading','peaches','peace','piece','east','west','many','special','beanstalk'];
 const PICTURE_OVERRIDES = ['peaches','peace','many','special','beanstalk'];
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./word-picture-sprite.png','./sound-the-word-capybara.png',...PICTURE_OVERRIDES.map(word=>`./word-pictures/${word}.png`),...DETECTIVE_WORDS.map(word=>`./word-detective/${word}.png`)];
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./word-picture-sprite.png','./sound-the-word-capybara.png','./sky-high-balloon.png',...PICTURE_OVERRIDES.map(word=>`./word-pictures/${word}.png`),...DETECTIVE_WORDS.map(word=>`./word-detective/${word}.png`)];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
