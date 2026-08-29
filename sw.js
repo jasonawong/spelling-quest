2be4ce7f92c36f822813eac092cd46cedfd5e497
Jason Wong <jasonwong@Jasons-MacBook-Air.local>
2026-08-28 20:03:38 -0400

    Update spelling words and clue artwork

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-const CACHE = 'spelling-quest-v54';
-const APP_VERSION = '54';
-const DETECTIVE_ART_VERSION = 48;
-const DETECTIVE_WORDS = ['wrench','hello','yellow','bread','kept','better','sentence','eggshell','medal','metal','remember','reading','peaches','peace','piece','east','west','many','special','beanstalk'];
-const PICTURE_OVERRIDES = ['peaches','peace','many','special','beanstalk'];
-const ASSETS = ['./','./index.html',`./island-quest.css?v=${APP_VERSION}`,`./island-quest.js?v=${APP_VERSION}`,'./vendor/three.module.min.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./word-picture-sprite.png','./sound-the-word-capybara.png','./sky-high-balloon.png',...PICTURE_OVERRIDES.map(word=>`./word-pictures/${word}.png`),...DETECTIVE_WORDS.map(word=>`./word-detective/${word}.png?v=${DETECTIVE_ART_VERSION}`)];
+const CACHE = 'spelling-quest-v56';
+const APP_VERSION = '56';
+const DETECTIVE_SPRITES = [1,2,3,4,5].map(number=>`./word-detective-sprites/set-${number}.png?v=${APP_VERSION}`);
+const ASSETS = ['./','./index.html',`./island-quest.css?v=${APP_VERSION}`,`./island-quest.js?v=${APP_VERSION}`,'./vendor/three.module.min.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./word-picture-sprite-v55.png','./sound-the-word-capybara.png','./sky-high-balloon.png',...DETECTIVE_SPRITES];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => Promise.all(ASSETS.map(url => cache.add(new Request(new URL(url,self.location).href,{ cache:'reload' }))))).then(()=>self.skipWaiting()));
 });
